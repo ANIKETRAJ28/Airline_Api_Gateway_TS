e99eb25af8f175bba9f3b2392d446196f2d81bc0
Aniket <aniketraj28042003@gmail.com>
2025-05-28 18:39:34 +0530

    update: middleware: updated booking proxy middleware

--- src/middleware/proxy.middleware.ts
+++ src/middleware/proxy.middleware.ts
@@ -1,6 +1,5 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { services } from '../util/services.util';
-import { NextFunction, Request, Response } from 'express';
 
 export const flightSearchProxy = createProxyMiddleware({
   target: services.SEARCH_SERVICE_URL,
@@ -18,13 +17,10 @@ export const authProxy = createProxyMiddleware({
   },
 });
 
-export const bookingProxy = (req: Request, res: Response, next: NextFunction) => {
-  const proxy = createProxyMiddleware({
-    target: services.BOOKING_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-      '^/reservation': '',
-    },
-  });
-  return proxy(req, res, next);
-};
+export const bookingProxy = createProxyMiddleware({
+  target: services.BOOKING_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/reservation': '',
+  },
+});
